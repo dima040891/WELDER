fecf5977822a2d3385cf72a72c91861d239bf316
LEV <dima040891@yandex.ru>
2021-12-11 15:46:13 +0300

    Проверка работы git на ноуте
    Закомментирован вызов шедулера из main.c

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -151,7 +151,7 @@ int main(void)
   /* USER CODE END RTOS_EVENTS */
 
   /* Start scheduler */
-  osKernelStart();
+//  osKernelStart();
 
   /* We should never get here as control is now taken by the scheduler */
   /* Infinite loop */
